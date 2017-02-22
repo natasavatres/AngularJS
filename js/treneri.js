0650ec7e0127c20273bbf2a51edb8519d939bce2
natasavatres <natasavatres@gmail.com>
2017-02-22 23:47:48 +0100

    Dodate funkcionalnosti dodaj u listu i izbrisi

--- js/treneri.js
+++ js/treneri.js
@@ -2,42 +2,78 @@
 var treneri = angular.module('treneri', []);
 
 treneri.controller('treneriController',['$scope', '$filter', '$http', function ($scope, $filter, $http) {
-                                        
+    
+     $scope.treneri = [
+    {
+      "ime": "Nata",
+      "prezime": "sajkfhla",
+    "email":"asjkfd@aljksd.com",
+      "telefon":1234567,
+    "ss": "sdgfsa",
+        "trening": "shafdba"
+        
+    }, {
+      "ime": "Anci",
+      "prezime": "sajkfhla",
+    "email":"asjkfd@aljksd.com",
+      "telefon":1234567,
+    "ss": "sdgfsa",
+        "trening": "shafdba"
+    }, {
+       "ime": "Vanja",
+      "prezime": "sajkfhla",
+    "email":"asjkfd@aljksd.com",
+      "telefon":1234567,
+    "ss": "sdgfsa",
+        "trening": "shafdba"
+    }
+];
+    
+    $scope.dodajTrenera = function(trener){
+        alert("Uspešno dodato :D");
+        $scope.treneri.push(trener);
+         $scope.current = {};
+    };
+    
+    
+    $scope.obrisiTrenera = function(trener){
+        var trenerZaBrisanje = $scope.treneri.indexOf(trener);
+        $scope.treneri.splice(trenerZaBrisanje, 1);
+    };
+    
+//    $scope.izmeniTrenera = function(trener){
+//        $scope.current = trener;
+//    };
+    
+//      $scope.sacuvajTrenera = function(trener){
+//            alert("Uspešno sačuvano :D");
+//          $scope.current = {};
+//    };
+    
+    $scope.current = {};
+
+//    alert pocinje
+    
     $scope.ime = '';
     $scope.prezime = '';
                                         
     $scope.najmanje = 2;
     $scope.najvise = 30;
     
-    $scope.alertClick = function() {
-        alert("Vaš unos je uspešno sačuvan :D");
-    };
+//    $scope.alertClick = function() {
+//        alert("Vaš unos je uspešno sačuvan :D");
+//    };
+    
+      
+    
     
     //------------------
-    $scope.treneri = [
-    {
-      ime: 'Nata',
-      prezime: 'sajkfhla',
-      email:'asjkfd@aljksd.com',
-      telefon:'1234567'
-        
-    }, {
-      ime: 'Anci',
-      prezime: 'sajkfhla',
-      email:'asjkfd@aljksd.com',
-      telefon:'1234567'
-    }, {
-      ime: 'Vanja',
-      prezime: 'sajkfhla',
-      email:'asjkfd@aljksd.com',
-      telefon:'1234567'
-    }
-  ];
-    //------------------
+    console.log(angular.toJson($scope.treneri));
     
     $http.get('/db.json')
         .success(function(podaciOTrenerima){
-            console.log(podaciOTrenerima);
+//            console.log(podaciOTrenerima);
+        $scope.treneri = podaciOTrenerima;
     })
         .error(function(podaci, status){
             console.log(podaci);
@@ -50,3 +86,30 @@ treneri.controller('treneriController',['$scope', '$filter', '$http', function (
 //        
 //    };
 }]);
+
+//var rute = angular.module('rute', []);
+//
+//rute.config(['$routeProvider',function($routeProvider){
+//    
+//  $routeProvider 
+//
+//  .when('/', {
+//    templateUrl: 'stranice/vizija.html', 
+//    controller: 'vizijaController'
+//  })
+//
+//  .when('/misija')
+//    templateUrl: 'stranice/misija.html', 
+//    controller: 'misijaController'
+//})
+//
+//});
+//
+//rute.controller('vizijaController',['$scope', function ($scope) {
+//    
+//  }]);
+//
+//
+//rute.controller('misijaController',['$scope', function ($scope) {
+//
+//  }]);
